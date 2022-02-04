fa60c3bea5337b4323a001b885bef08cd2a2c564
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-02-04 10:11:58 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, X-Requested-With");
 
-$serverhost = "hirayatech.net";
+$serverhost = "ns1.dns-parking.com";
 $username = "u135501428_hirayatechdev";
 $password = "Hirayatech2022";
 $dbname = "u135501428_mapha_trucking";
