5306e768dbd686e285a7e0aeb93abc0188447ced
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-02-12 21:59:18 +0800

    Add Worker Filtering

---
+++ api/worker/get_active_drivers.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('../../config.php');
+
+try {
+
+    $emparray = array();
+
+    $sql = "SELECT * FROM tblworker WHERE active=1 && position='Driver' ORDER BY id_no";
+    $result = $db->query($sql);
+
+    // output data of each row
+
+    while($row = $result->fetch_assoc()) {
+        $emparray[] = $row;
+    }
+    
+    echo json_encode($emparray);
+
+} catch (exception $e) {
+    echo json_encode($emparray);
+}
+
+?>
